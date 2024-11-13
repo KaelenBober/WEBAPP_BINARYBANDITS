f7ac60330f035c04a19b8604d954859a5e13701f
MaxxUribe <166677133+MaxxUribe@users.noreply.github.com>
2024-11-13 02:40:03 +0000

    fixed shit

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('title_screen.html')
 
+@app.route('/login')
+def login():
+    return render_template('login_page.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
